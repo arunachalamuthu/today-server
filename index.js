bcd5912fe761d6024b6d82b5370912e8ebdc4e38
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-05 02:57:22 +0530

    ss

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import cors from 'cors'
-// import { fileURLToPath } from 'url';
-// import path from 'path'
-// import { dirname } from "path"
+import { fileURLToPath } from 'url';
+import path from 'path'
+import { dirname } from "path"
 const app=express()
 
 
 
 
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 app.use(express.json())
@@ -18,7 +18,11 @@ app.use(cors())
 // app.use(express.static(path.join(__dirname+'/public/client')))
 // app.use(express.static(path.join(__dirname+'/public' )))
 app.get('/',(req,res)=>{
-res.send('welcome ')
+// res.send('welcome ')
+
+app.use(express.static(path.resolve(__dirname,'public','client')))
+res.sendFile(path.resolve(__dirname,'public','client','index.html'))
+
 }
 )
 
