9355712f4ac70da0268a5c2ef14fdbb2cef2c62b
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-05 03:50:12 +0530

    f

---
+++ index2.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import { fileURLToPath } from 'url';
+import path from 'path'
+import { dirname } from "path"
+const app=express()
+
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+app.use(express.json())
+app.use(cors())
+// app.use(express.static(path.join(__dirname+'/public/client')))
+// app.use(express.static(path.join(__dirname+'/public' )))
+app.get('/',(req,res)=>{
+// res.send('welcome ')
+
+app.use(express.static(path.resolve(__dirname,'public','client2')))
+res.sendFile(path.resolve(__dirname,'public','client2','index.html'))
+
+}
+)
+app.get('/api',(req,res)=>{
+    res.json('hi')
+})
+
+app.listen(process.env.PORT || 4001,()=>{
+    console.log('work')
+})
