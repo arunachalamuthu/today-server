e8e59413bc113d40ebe05744f5cb4986ee763ecd
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-04 22:07:30 +0530

    upload login

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from 'express'
 import cors from 'cors'
+import { fileURLToPath } from 'url';
+import path from 'path'
+import { dirname } from "path"
 const app=express()
 
-app.use(cors())
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 app.use(express.json())
-
-app.get('/',(req,res)=>{
-res.send('welcome ')
-}
-)
+app.use(cors())
+app.use(express.static(path.join(__dirname+'/public/client')))
+// app.use(express.static(path.join(__dirname+'/public' )))
+// app.get('/',(req,res)=>{
+// res.send('welcome ')
+// }
+// )
 
 app.listen(process.env.PORT || 4000,()=>{
     console.log('work')
